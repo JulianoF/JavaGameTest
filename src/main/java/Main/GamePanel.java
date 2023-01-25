b94741d555b3d89e81e3d09a83e47bf79dd30697
JulianoF <juliano1@live.ca>
2023-01-25 10:49:20 -0500

    Initial Commit

---
+++ src/main/java/Main/GamePanel.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+/**
+ *
+ * @author juliano
+ */
+public class GamePanel extends JPanel{
+    public static final int HEIGHT = 600;
+    public static final int WIDTH = 800;
+    public static final int SCALE = 2;
+    
+    private Thread gameThread;
+    private Graphics gphs;
+    private boolean running;
+    
+    public GamePanel(){
+        setBackground(Color.white);
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setFocusable(true);
+        requestFocus();
+    }
+
+
+}
